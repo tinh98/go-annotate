6e3757e7a06c1433dd2f9300b5aa24e02276dfaa
tinh98 <>
2023-02-27 12:55:35 +0700

    test

--- example/user_controller.go
+++ example/user_controller.go
@@ -5,35 +5,17 @@ import (
 	"github.com/kataras/iris/v12/mvc"
 )
 
+//go:generate go-annotate -p "example" -f "user_controller.go"
+
 type UserController struct {
 }
 
-// Get
-// @Summary TODO
-// @Description TODO
-// @Tags TODO
-// @Accept json
-// @Produce json
-// @Param usercontroller path string true "UserController ID"
-// @Success 200 {object} &{mvc Result}
-// @Router /usercontroller/ [get]
 func (c UserController) Get() mvc.Result {
 	return mvc.Response{
 		Object: iris.Map{"userID": "123"},
 	}
 }
 
-// GetList
-// @Summary TODO
-// @Description TODO
-// @Tags TODO
-// @Accept json
-// @Produce json
-// @Param usercontroller path string true "UserController ID"
-// @Param ctx body &{iris true "ctx"
-// @Param ctx query  true "ctx"
-// @Success 200 {object} &{mvc Result}
-// @Router /usercontroller/List [get]
 func (c UserController) GetList(ctx iris.Context) mvc.Result {
 	folderPath := ctx.URLParam("folderPath")
 	return mvc.Response{
--- main.go
+++ main.go
@@ -1,9 +1,58 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"go-annotate/cmd"
+	"go-annotate/pkg/goan"
+	"os"
+	"os/exec"
+	"path"
+	"strings"
 )
+
+var (
+	pkgName   string
+	fileName  string
+	toolType  string
+	outputDir string
+)
+
+func init() {
+	flag.StringVar(&pkgName, "p", "", "The package name")
+	flag.StringVar(&fileName, "f", "", "The file name")
+	flag.StringVar(&toolType, "t", "swagger", "The tool type (default: swagger)")
+	flag.StringVar(&outputDir, "o", "", "The output directory")
+}
 
 func main() {
 	cmd.Execute()
+	flag.Parse()
+
+	if pkgName == "" || fileName == "" {
+		flag.Usage()
+		os.Exit(1)
+	}
+
+	args := []string{
+		"go-annotate",
+		fmt.Sprintf("-p %s", pkgName),
+		fmt.Sprintf("-f %s", fileName),
+		fmt.Sprintf("-t %s", toolType),
+	}
+
+	if outputDir != "" {
+		args = append(args, fmt.Sprintf("-o %s", outputDir))
+	}
+
+	cmd := exec.Command("go", "generate", strings.Join(args, " "))
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	if err := cmd.Run(); err != nil {
+		fmt.Fprintf(os.Stderr, "Error running go-annotate: %s\n", err)
+		os.Exit(1)
+	}
+
+	goan.GenerateSwaggerAnnotations(path.Join(fileName, pkgName))
 }
